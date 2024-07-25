ad35e2c4bf7767844224af6c949f3cbe9324ac8e
Tanmay Singla <tanmay1903@hotmail.com>
2024-07-24 19:15:24 -0700

    Initial Commit

---
+++ fetchAssessment/fetchAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchAssessmentApp.swift
+//  fetchAssessment
+//
+//  Created by Tanmay Singla on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
